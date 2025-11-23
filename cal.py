181ebf14e0c01680188ba8ecb04f843496a8661d
Rameshwari Shinde <shinderameshwari418@gmail.com>
2025-11-23 11:09:29 +0530

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.set_page_config(page_title="Simple Calculator")
+
+st.title("Simple Calculator")
+
+# Input numbers
+num1 = st.number_input("Enter first number:", value=0.0)
+num2 = st.number_input("Enter second number:", value=0.0)
+
+# Operations
+operation = st.selectbox(
+    "Select Operation",
+    ["Add", "Subtract", "Multiply", "Divide"]
+)
+
+# Calculate
+if st.button("Calculate"):
+    if operation == "Add":
+        result = num1 + num2
+    elif operation == "Subtract":
+        result = num1 - num2
+    elif operation == "Multiply":
+        result = num1 * num2
+    elif operation == "Divide":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error: Cannot divide by 0"
+
+    st.success(f"Result = {result}")
